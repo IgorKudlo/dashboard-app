f98d14e8659fb5ca652720ea95cb56000a41aed4
igor.kudlo <igor.kudlo@spiralscout.com>
2022-06-08 15:53:47 +0300

    logic add filters

--- src/store/filters/filters-reducer.js
+++ src/store/filters/filters-reducer.js
@@ -1,10 +1,12 @@
-import {defaultMergeProps} from 'react-redux/lib/connect/mergeProps';
 import {ADD_FILTER, CLEAR_FILTER} from './filters-actions';
 
 export const filterReducer = (state = [], {type, payload}) => {
     switch (type) {
         case ADD_FILTER: {
-            return [...state, payload.filter];
+            if (!state.includes(payload.filter)) {
+                return [...state, payload.filter];
+            }
+            return state;
         }
         case CLEAR_FILTER: {
             return state.filter(item => item !== payload.filter)
